8d167e3a6ccca0c3d08712a708496ccee4007ad0
thugiang <xanhtham.cuc@gmail.com>
2022-09-20 15:31:18 +1200

    first commit

---
+++ pages/api/post/index.ts
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { allPostsQuery } from "../../../utils/utils/queries";
+
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    if (req.method === "GET") {
+        const query = allPostsQuery();
+  }
+}
